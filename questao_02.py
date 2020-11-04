01c4a334e4470763935174edd62b8ae98560a96b
franciscocamellon <franciscocamellon@gmail.com>
2020-11-04 18:14:49 -0300

    Finished

--- questao_02.py
+++ questao_02.py
@@ -16,24 +16,27 @@ class Questao_02():
 
     def __init__(self):
         """ Constructor. """
-
+        self.title = 'Digite um número inteiro: '
+        self.my_list = []
 
     def init_class(self):
         """ This function receives the input data from users. """
-
-
+        while len(self.my_list) <= 5:
+            _input = Validate().validate_values(self.title, zero=True)
+            self.my_list.append(_input)
 
     def process_data(self):
         """ This function process the input data from init_class. """
-
-
-
+        self.init_class()
+        
     def print_result(self):
         """ This is a printer! It prints. """
 
         print('===' * 25, 'Questão 02'.center(75), '===' * 25, sep='\n')
         self.process_data()
-        print('---' *25, '{}','---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep="\n")
+        print('---' * 25, '{}'.format(self.my_list),
+              '---' * 25,
+              'Aluno: Francisco Camello'.rjust(75), sep="\n")
 
 
 Questao_02().print_result()
