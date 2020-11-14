4a73c7532d59b48fa34148bfe129050728b9d73a
franciscocamellon <franciscocamellon@gmail.com>
2020-11-14 13:14:21 -0300

    Finished

--- questao_04.py
+++ questao_04.py
@@ -10,29 +10,41 @@
 """
 from validation import Validate
 
+
 class Questao_04():
     """ Docstring """
 
     def __init__(self):
         """ Constructor. """
-
+        self.title1 = 'Digite o tamanho da lista: '
+        self.title2 = 'Digite um número: '
+        self.list = []
+        self.length = 0
 
     def init_class(self):
         """ This function receives the input data from users. """
-
-
+        self.length = Validate().validate_values(self.title1, zero=False)
+        while len(self.list) < self.length:
+            self.list.append(Validate().validate_numbers(self.title2))
 
     def process_data(self):
         """ This function process the input data from init_class. """
-
-
+        self.init_class()
+        l = [value for value in self.list if value == 0]
+        self.list.clear()
+        self.list = l
 
     def print_result(self):
         """ This is a printer! It prints. """
-
         print('===' * 25, 'Questão 04'.center(75), '===' * 25, sep='\n')
         self.process_data()
-        print('---' *25, '{}','---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep="\n")
+        print('---' * 25)
+        if len(self.list) > 0:
+            print('Existem {} números iguais a zero nesta lista'.format(len(self.list)),
+              '---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep="\n")
+        else:
+            print('Não existem números iguais a zero nesta lista',
+              '---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep="\n")
 
 
 Questao_04().print_result()
