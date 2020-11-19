b5c314304ce40575baed3ccbb6f032050cbaabee
franciscocamellon <franciscocamellon@gmail.com>
2020-11-19 10:25:58 -0300

    Finished

--- questao_10.py
+++ questao_10.py
@@ -8,32 +8,65 @@
 *        Nome do arquivo : questao_10.py                                   *
 ***************************************************************************/
 """
-from validation import Validate
+import pygame
+
 
 class Questao_10():
     """ Docstring """
 
     def __init__(self):
         """ Constructor. """
+        pygame.init()
+        self.DISPLAY_NAME = pygame.display.set_caption('Questão 10')
+        self.KEY = pygame.key.get_pressed()
+        self.SCREEN_WIDTH = 400
+        self.SCREEN_HEIGHT = 400
+        self.SCREEN = pygame.display.set_mode(
+            (self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        self.RECT = pygame.Rect(
+            (self.SCREEN_WIDTH * 3/8, self.SCREEN_HEIGHT * 3/8), (100, 100))
+        self.FPS = 60
+        self.FPSCLOCK = pygame.time.Clock()
+        self.RED = (255, 0, 0)
+        self.BLACK = (0, 0, 0)
+        self.finish = False
+
+    def move_keys(self, rect):
+        """ This functions moves a rectangle in place"""
+        if self.KEY[pygame.K_LEFT]:
+            rect.move_ip(-10, 0)
+        if self.KEY[pygame.K_RIGHT]:
+            rect.move_ip(10, 0)
+        if self.KEY[pygame.K_UP]:
+            rect.move_ip(0, -10)
+        if self.KEY[pygame.K_DOWN]:
+            rect.move_ip(0, 10)
+
+    def draw_rectangle(self, surface, color, rect):
+        """ This functions draws a rectangle """
+        pygame.draw.rect(surface, color, rect)
+
+    def init_game(self):
+        """ Docstring """
+
+        while not self.finish:
+            # Checar os eventos do mouse aqui:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.finish = True
+
+            self.SCREEN.fill(self.BLACK)
+
+            self.draw_rectangle(
+                self.SCREEN, self.RED, self.RECT)
+            self.move_keys(self.RECT)
+            # Atualiza o desenho na tela
+            pygame.display.update()
+
+            # 60 frames por segundo
+            self.FPSCLOCK.tick(self.FPS)
+        # Finaliza a janela
+        pygame.display.quit()
 
 
-    def init_class(self):
-        """ This function receives the input data from users. """
-
-
-
-    def process_data(self):
-        """ This function process the input data from init_class. """
-
-
-
-    def print_result(self):
-        """ This is a printer! It prints. """
-
-        print('===' * 25, 'Questão 10'.center(75), '===' * 25, sep='\n')
-        self.process_data()
-        print('---' *25, '{}','---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep="\n")
-
-
-
-Questao_10().print_result()
+Questao_10().init_game()
