f3571c055fdcc2b98306c37ecf0f33206871da3c
franciscocamellon <franciscocamellon@gmail.com>
2020-11-19 09:17:44 -0300

    Finished

--- questao_09.py
+++ questao_09.py
@@ -9,30 +9,49 @@
 ***************************************************************************/
 """
 from validation import Validate
+import pygame
+
 
 class Questao_09():
     """ Docstring """
 
     def __init__(self):
         """ Constructor. """
+        pygame.init()
+        self.DISPLAY_NAME = pygame.display.set_caption('Questão 09')
+        self.SCREEN_WIDTH = 400
+        self.SCREEN_HEIGHT = 400
+        self.SCREEN = pygame.display.set_mode(
+            (self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        self.FPS = 60
+        self.FPSCLOCK = pygame.time.Clock()
+        self.BLUE = (0, 0, 255)
+        self.finish = False
 
 
-    def init_class(self):
-        """ This function receives the input data from users. """
+    def draw_circle(self, surface, color, position, radius):
+        """ This functions draws a circle """
+        pygame.draw.circle(surface, color, position, radius)
+
+    def init_game(self):
+        """ Docstring """
+
+        while not self.finish:
+            # Checar os eventos do mouse aqui:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.finish = True
+
+            self.draw_circle(
+                self.SCREEN, self.BLUE, (self.SCREEN_WIDTH // 2, self.SCREEN_HEIGHT // 2), 100)
+
+            # Atualiza o desenho na tela
+            pygame.display.update()
+
+            # 60 frames por segundo
+            self.FPSCLOCK.tick(self.FPS)
+        # Finaliza a janela
+        pygame.display.quit()
 
 
-
-    def process_data(self):
-        """ This function process the input data from init_class. """
-
-
-
-    def print_result(self):
-        """ This is a printer! It prints. """
-
-        print('===' * 25, 'Questão 09'.center(75), '===' * 25, sep='\n')
-        self.process_data()
-        print('---' *25, '{}','---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep="\n")
-
-
-Questao_09().print_result()
+Questao_09().init_game()
