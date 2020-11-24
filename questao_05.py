4a86d4e577fad324b22389e77de87c87c0a9a8e6
franciscocamellon <franciscocamellon@gmail.com>
2020-11-23 23:29:07 -0300

    Finished

--- questao_05.py
+++ questao_05.py
@@ -9,39 +9,47 @@
 ***************************************************************************/
 """
 
-from validation import Validate
-
 
 class Questao_05():
     """ Docstring """
 
     def __init__(self):
         """ Constructor. """
-        self.input = {"Digite o nome do aluno: ":[],
-        "Digite a altura do aluno: ":[]}
-
+        self.students = []
+        self.average = 0
+        self.out_of_average = []
 
     def init_class(self):
         """ This function receives the input data from users. """
-        while "sair" not in self.input["Digite o nome do aluno: "]:
-            for k, v in self.input.items():
-                self.input["Digite o nome do aluno: "].append(input(k))
-                self.input["Digite a altura do aluno: "].append(input(k))
-
-
+        finish = False
+        while not finish:
+            student = input('Digite o nome do aluno: ')
+            if student != 'sair':
+                height = float(input('Digite a altura do aluno: '))
+                self.students.append([student, height])
+            else:
+                finish = True
 
     def process_data(self):
         """ This function process the input data from init_class. """
         self.init_class()
+        _sum = 0
 
+        for i in range(len(self.students)):
+            _sum += float(self.students[i][1])
+        self.average = _sum / len(self.students)
 
+        for i in range(len(self.students)):
+            if float(self.students[i][1]) > self.average:
+                self.out_of_average.append(self.students[i][0])
 
     def print_result(self):
         """ This is a printer! It prints. """
 
         print('===' * 25, 'Questão 05'.center(75), '===' * 25, sep='\n')
         self.process_data()
-        print('---' *25, '{}'.format(self.input),'---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep="\n")
+        print('---' * 25, 'Média das alturas {} metros'.format(round(self.average, 2)),
+              'Alunos acima da média: {0}'.format(self.out_of_average), '---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep="\n")
 
 
 Questao_05().print_result()
